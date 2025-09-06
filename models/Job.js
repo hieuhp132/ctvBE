436fa9fc49a9f0ae1b7558eb4b14488ac7a047fb
hieuhp132 <hieuhp132@gmail.com>
2025-09-06 11:18:20 +0700

    fix submit all filled informations from frontend process

--- models/Job.js
+++ models/Job.js
@@ -6,6 +6,11 @@ const jobSchema = new mongoose.Schema({
   location: { type: String, required: true },
   bonus: { type: Number, default: 0 },
   deadline: { type: Date },
+  rewardCandidateUSD: { type: Number, default: 0 },
+  rewardInterviewUSD: { type: Number, default: 0 },
+  vacancies: { type: Number, default: 0 },
+  applicants: { type: Number, default: 0 },
+  status: { type: String, default: "Active" },
   jobsdetail: {
     description: { type: String, default: "" },
     requirement: { type: String, default: "" },
