d494f38edcf1a4b5b1b77f8ae4612e977d2dc47f
hieuhp132 <hieuhp132@gmail.com>
2025-10-08 19:04:18 +0700

    remove waiting time

--- controllers/referral.js
+++ controllers/referral.js
@@ -65,7 +65,6 @@ exports.createReferral = async (req, res) => {
       bonus: Number(bonus) || 0,
       message: message || "",
     });
-    await delay(1000);
     res.status(201).json(referral);
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err.message });
